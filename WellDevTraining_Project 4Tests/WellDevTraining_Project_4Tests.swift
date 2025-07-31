429e5fd6ada16235faa2b46e08ec40ba1c752ada
Ehsanul Karim <ehsanul.karim@welldev.io>
2025-07-31 10:41:06 +0600

    Part 2 Done

---
+++ WellDevTraining_Project 4Tests/WellDevTraining_Project_4Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WellDevTraining_Project_4Tests.swift
+//  WellDevTraining_Project 4Tests
+//
+//  Created by Ehsanul Karim on 31/7/25.
+//
+
+import Testing
+@testable import WellDevTraining_Project_4
+
+struct WellDevTraining_Project_4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
